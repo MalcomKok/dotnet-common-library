41b3fd2465296e0747f2b0fc4fa7e79a335196c8
Malcom Kok <malcom@myonlinepresence.co.za>
2021-05-29 20:32:29 +0200

    Add InterviewCodeChallengeExtension class
    - Initial adding of new extension class
    - Added FizzBuzz code

---
+++ DotnetCommonLibrary/Extensions/InterviewCodeChallengeExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DotnetCommonLibrary.Extensions
+{
+    public static class InterviewCodeChallengeExtensions
+    {
+        public static void FizzBuzz(this List<int> intList, int firstMultiple, int secondMultiple) => intList.Select(x => FizzBuzz(x, firstMultiple, secondMultiple));
+
+        public static string FizzBuzz(this int intValue, int firstMultiple, int secondMultiple)
+        {
+            var isFirstMultipleValid = intValue % firstMultiple == 0;
+            var isSecondMultipleValid = intValue % secondMultiple == 0;
+
+            if (isFirstMultipleValid && isSecondMultipleValid)
+                return "FIZZBUZZ";
+            else if (isFirstMultipleValid)
+                return "FIZZ";
+            else if (isSecondMultipleValid)
+                return "BUZZ";
+            else
+                return $"{intValue}";
+        }
+    }
+}
